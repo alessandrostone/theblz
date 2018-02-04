7d39f784a2495b6eac71c85b1a7edc83cdd84cfc
Glitch (friendly-range) <none>
2018-02-04 04:39:38 +0000

    🌯🚘 Updated with Glitch

--- client/index.js
+++ client/index.js
@@ -4,31 +4,14 @@ const glsl = require('glslify');
 const vertexShader = glsl.file('../shaders/vertex.glsl');
 const fragmentShader = glsl.file('../shaders/fragment.glsl');
 //const Tone = require('Tone');
+//var player = require("Tone").Player;
 
-var players = {};
+var theplayer = require("Tone").player;
+Tone.Transport.loop = true;
+Tone.Transport.start(0);
+console.log("sanity check");
 
-var fileEnding = ".mp3";
-var parts = [
-    "bass",
-    //"bell",
-    //"high hit",
-    //"low hit",
-    "reverb"
-];
-/** var baseUrl = "https://andrescuervo.github.io/assets/chamber/mp3/";
-console.log("PARTSssss", parts);
-parts.forEach(function (part) {
-  //console.log(baseUrl + part + fileEnding);
-  var player = new Tone.Player({
-  url: encodeURI(baseUrl + part + fileEnding),
-  loop: true
-}).toMaster().sync().start(0);
-  players[part] = player;
-}
-);
-**/
-
-var player = new Tone.Player({
+/**var player = new Tone.Player({
   url: "https://cdn.glitch.com/e6e85513-e78c-4f1f-a548-e117d0e514d4%2Floop.mp3?1517704399354",
   loop: true
 }).toMaster().sync().start(0);
@@ -36,22 +19,23 @@ var player = new Tone.Player({
 Tone.Transport.loop = true;
 Tone.Transport.start(0);
 console.log("sanity check");
+**/
 
 
 
-
-AFRAME.registerComponent('displace-model', {
+AFRAME.registerComponent('funktion', {
   init: function () {
     
     // TODO : Replace this with accessing the global Buffer's bass.wav
     this.player = new Tone.Player({
         url: "https://cdn.glitch.com/e6e85513-e78c-4f1f-a548-e117d0e514d4%2Floop.mp3?1517704399354",
         loop: true
-      }).toMaster();
+        
+      }).toMaster().sync().start(0);
     
-    //this.player.autostart = true;
+    this.player.autostart = true;
     
-    this.player = player;
+    //this.player = player;
     this.player.playbackRate /= 1;
     
     this.meter = new Tone.Meter("level");
@@ -74,6 +58,7 @@ AFRAME.registerComponent('displace-model', {
   tick: function (t) {
     var val = this.meter.value;
     var currentPosition = this.el.object3D.position;
-    this.el.object3D.position.set(currentPosition.x, currentPosition.y, val * 25);
+    this.el.object3D.position.set(currentPosition.x, currentPosition.y, val * -32);
   }
 })
+
