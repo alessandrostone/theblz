da4558f844159d34f88f2cdf4da25f763d7ab9e4
Glitch (friendly-range) <none>
2018-02-04 01:15:19 +0000

    💺😇 Updated with Glitch

--- client/index.js
+++ client/index.js
@@ -15,7 +15,7 @@ var parts = [
     //"low hit",
     "reverb"
 ];
-var baseUrl = "https://andrescuervo.github.io/assets/chamber/mp3/";
+/** var baseUrl = "https://andrescuervo.github.io/assets/chamber/mp3/";
 console.log("PARTSssss", parts);
 parts.forEach(function (part) {
   //console.log(baseUrl + part + fileEnding);
@@ -26,109 +26,18 @@ parts.forEach(function (part) {
   players[part] = player;
 }
 );
+**/
+
+var player = new Tone.Player({
+  url: "https://cdn.glitch.com/e6e85513-e78c-4f1f-a548-e117d0e514d4%2Floop.mp3?1517704399354",
+  loop: true
+}).toMaster().sync().start(0);
 
 Tone.Transport.loop = true;
 Tone.Transport.start(0);
 console.log("sanity check");
 
-AFRAME.registerComponent('scale-on-bell', {
-  /**
-   * Creates a new THREE.ShaderMaterial using the two shaders defined
-   * in vertex.glsl and fragment.glsl.
-   */
-  init: function () {
-    // this.material  = new THREE.ShaderMaterial({
-    //   uniforms: {
-    //     time: { value: 0.0 },
-    //     displaceBy: {value : 1.0},
-    //     opacity: {value : 1.0}
-    //   }      ,
-    //   vertexShader,
-    //   fragmentShader
-    // });
-    // this.el.addEventListener('model-loaded', () => this.update());    
-    
-    this.player = players["bell"];
-    this.meter = new Tone.Meter("level");
-    this.player.connect(this.meter);  
-  },
 
-  /**
-   * Apply the material to the current entity.
-   */
-  update: function () {
-    // const mesh = this.el.getObject3D('mesh');
-    // if (mesh) {
-    //   mesh.material = this.material;
-    // }
-  },
-
-  /**
-   * On each frame, update the 'time' uniform in the shaders.
-   */
-  tick: function (t) {
-    let s = this.meter.value * 15
-    //this.el.setAttribute('scale', `${s} ${s} ${s}`)
-    // this.el.setAttribute('color', `hsl(240, 60%, ${val * 10}%)`)
-    // this.el.setAttribute('color', `hsl(240, 60%, ${Math.floor(this.meter.value * 15)}%)`)
-   this.el.setAttribute('color', `hsl(${Math.floor(360 / val)}, 60%, 80%)`)
-  // this.el.setAttribute('color', `hsl(240, 60%, ${Math.floor(80 - (val * 4))}%)`)
-    // console.log("%cscale-on-bell's components new scale value: " + val, `color: hsl( 340, 80%, ${val * 10}%)`)
-  }
-  
-})
-
-AFRAME.registerComponent('material-displacement', {
-  /**
-   * Creates a new THREE.ShaderMaterial using the two shaders defined
-   * in vertex.glsl and fragment.glsl.
-   */
-  init: function () {
-    this.material  = new THREE.ShaderMaterial({
-      uniforms: {
-        time: { value: 0.0 },
-        displaceBy: {value : 1.0},
-        opacity: {value : 1.0}
-      }      ,
-      vertexShader,
-      fragmentShader
-    });
-    this.el.addEventListener('model-loaded', () => this.update());    
-    
-    this.player = players["bass"];
-    this.meter = new Tone.Meter("level");
-    this.player.connect(this.meter);  
-  },
-
-  /**
-   * Apply the material to the current entity.
-   */
-  update: function () {
-    const mesh = this.el.getObject3D('mesh');
-    if (mesh) {
-      mesh.material = this.material;
-    }
-  },
-
-  /**
-   * On each frame, update the 'time' uniform in the shaders.
-   */
-  tick: function (t) {
-    this.material.uniforms.time.value = t / 1000;
-    
-    
-    var val = this.meter.value * 1005;
-    this.el.object3D.scale.set(val,val,val);
-    this.material.uniforms.displaceBy.value = val * 10;
-    this.material.uniforms.opacity.value = val * 10.55;
-    
-    //console.log(val);
-    var colorObject = this.el.components['material'].material.color;
-    colorObject['g'] = val * 0.6;
-    colorObject['b'] = val * 0.6;
-  }
-  
-})
 
 
 AFRAME.registerComponent('displace-model', {
@@ -136,13 +45,13 @@ AFRAME.registerComponent('displace-model', {
     
     // TODO : Replace this with accessing the global Buffer's bass.wav
     this.player = new Tone.Player({
-        url: "https://andrescuervo.github.io/assets/chamber/parts/bass.wav",
+        url: "https://cdn.glitch.com/e6e85513-e78c-4f1f-a548-e117d0e514d4%2Floop.mp3?1517704399354",
         loop: true
       }).toMaster();
     
     //this.player.autostart = true;
     
-    this.player = players["bass"];
+    this.player = player;
     this.player.playbackRate /= 1;
     
     this.meter = new Tone.Meter("level");
